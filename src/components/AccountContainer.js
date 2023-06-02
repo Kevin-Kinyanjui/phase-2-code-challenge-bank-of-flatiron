bc3482e47f83febf4ab5fe32aa622b3b5384f2ea
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-06-02 16:20:55 +0300

    first commit

---
+++ src/components/AccountContainer.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import TransactionsList from "./TransactionsList";
+import Search from "./Search";
+import AddTransactionForm from "./AddTransactionForm";
+
+function AccountContainer() {
+  const [transactions, setTransactions] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setTransactions(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  function addTransaction(newTransaction) {
+    setTransactions((prevTransactions) => [
+      ...prevTransactions,
+      newTransaction,
+    ]);
+  }
+
+  return (
+    <div>
+      <Search />
+      <AddTransactionForm addTransaction={addTransaction} />
+      <TransactionsList transactions={transactions} />
+    </div>
+  );
+}
+
+export default AccountContainer;
